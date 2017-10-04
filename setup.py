60d2ddfe538e9103285a31f12bb7002691b0eda2
Graham Nott <gnott@starglobal.ca>
2017-10-03 17:47:24 -0700

    Basic initial requirements, config, test scripts.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+import elifepubmed
+
+with open('README.rst') as fp:
+    readme = fp.read()
+
+setup(name='elifepubmed',
+    version=elifepubmed.__version__,
+    description='eLife PubMed deposit of journal articles.',
+    long_description=readme,
+    packages=['elifepubmed'],
+    license = 'MIT',
+    install_requires=[
+        "elifetools",
+        "elifearticle",
+        "GitPython",
+        "configparser"
+    ],
+    url='https://github.com/elifesciences/elife-pubmed-xml-generation',
+    maintainer='eLife Sciences Publications Ltd.',
+    maintainer_email='py@elifesciences.org',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        ]
+    )

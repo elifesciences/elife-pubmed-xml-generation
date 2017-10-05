3861eeddd00b20ee06959eb82efb687d286b197a
Graham Nott <gnott@starglobal.ca>
2017-10-05 10:50:00 -0700

    Test case for empty contributors for more test coverage.

--- tests/test_generate_edge_cases.py
+++ tests/test_generate_edge_cases.py
@@ -1,6 +1,6 @@
 import unittest
 from elifepubmed import generate
-from elifearticle.article import Article
+from elifearticle.article import Article, Contributor
 
 class TestGenerateArticleTitle(unittest.TestCase):
 
@@ -34,6 +34,25 @@ class TestGenerateArticleType(unittest.TestCase):
         # A quick test just look for the expected string in the output
         self.assertTrue(expected_fragment in pubmed_xml_string)
 
+
+class TestGenerateArticleContributors(unittest.TestCase):
+
+    def test_generate_blank_contributors(self):
+        "build an article object, set the contributors, generate PubMed XML"
+        doi = "10.7554/eLife.00666"
+        title = "Test article"
+        article = Article(doi, title)
+        contributor1 = Contributor(contrib_type="author", surname=None, given_name=None)
+        article.add_contributor(contributor1)
+        contributor2 = Contributor(contrib_type="author non-byline", surname=None, given_name=None)
+        article.add_contributor(contributor2)
+        # generate the PubMed XML
+        pXML = generate.build_pubmed_xml([article])
+        pubmed_xml_string = pXML.output_XML()
+        self.assertIsNotNone(pubmed_xml_string)
+        # A quick test just look for the expected string in the output
+        self.assertTrue('<AuthorList>' not in pubmed_xml_string)
+        self.assertTrue('<GroupList>' not in pubmed_xml_string)
 
 if __name__ == '__main__':
     unittest.main()

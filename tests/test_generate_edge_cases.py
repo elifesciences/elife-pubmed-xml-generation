7bad1204511c56ebbd30a0a22aa3d7ff4af7284b
Graham Nott <gnott@starglobal.ca>
2017-10-05 10:24:39 -0700

    Test case for article_type editorial producing PublicationType EDITORIAL.

--- tests/test_generate_edge_cases.py
+++ tests/test_generate_edge_cases.py
@@ -18,5 +18,22 @@ class TestGenerateArticleTitle(unittest.TestCase):
         self.assertTrue(expected_title in pubmed_xml_string)
 
 
+class TestGenerateArticleType(unittest.TestCase):
+
+    def test_generate_article_type(self):
+        "build an article object, set the article_type, generate PubMed XML"
+        doi = "10.7554/eLife.00666"
+        title = "Test article"
+        expected_fragment = "<PublicationType>EDITORIAL</PublicationType>"
+        article = Article(doi, title)
+        article.article_type = "editorial"
+        # generate the PubMed XML
+        pXML = generate.build_pubmed_xml([article])
+        pubmed_xml_string = pXML.output_XML()
+        self.assertIsNotNone(pubmed_xml_string)
+        # A quick test just look for the expected string in the output
+        self.assertTrue(expected_fragment in pubmed_xml_string)
+
+
 if __name__ == '__main__':
     unittest.main()
